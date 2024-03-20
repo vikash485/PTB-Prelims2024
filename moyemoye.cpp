4373da87e55595fcb3f89eecf115b21a3a27c1bb
Vikash <114328709+vikash485@users.noreply.github.com>
2024-03-20 08:33:04 +0530

    Add files via upload

---
+++ moyemoye.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+//less_equal to ordered multiset
+
+
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+    int ans=max(a,b)-min(a,b)+1;
+    cout<<ans<<endl;
+    // for(int i=1;i<=10000;i++)
+    // {
+    //     if(a+i>b && b+i>a && a+b>i)
+    //     {
+    //         cout<<i<<endl;
+    //         return;
+    //     }
+    // }
+}
+
+signed main(){
+    FastIO;
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    int t=1;
+    // cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        //cout<<"Case #"<<i<<": ";
+        solve();
+    }
+    return 0;
+}
